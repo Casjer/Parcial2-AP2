c2fb8408b1091b7556f03f1bead586096c2426c6
Asus <Asus@DESKTOP-PJ1BUTJ>
2021-06-30 18:33:36 -0400

    Cambiando tipo de dato "double" a "Decimal"

--- Models/Ventas.cs
+++ Models/Ventas.cs
@@ -12,8 +12,9 @@ namespace Parcial2.Models
         public int VentaId { get; set; }
         public DateTime Fecha { get; set; }
         public int ClienteId { get; set; }
-        public double Monto { get; set; }
-        public double Balance { get; set; }
-        
+        public decimal Monto { get; set; }
+        public decimal Balance { get; set; }
+        public virtual bool Pendiente => Balance > 0;
+
     }
 }
