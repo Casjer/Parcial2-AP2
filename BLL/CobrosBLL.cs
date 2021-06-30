be155dc18c0a267406b9b9057189de7d98c93397
Asus <Asus@DESKTOP-PJ1BUTJ>
2021-06-30 18:30:56 -0400

    Listo CobrosBLL

--- BLL/CobrosBLL.cs
+++ BLL/CobrosBLL.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2.DAL;
+using Parcial2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,184 @@ namespace Parcial2.BLL
 {
     public class CobrosBLL
     {
+        public async static Task<bool> Guardar(Cobros cobro)
+        {
+            return await Insertar(cobro);
+        }
+
+        public async static Task<bool> Insertar(Cobros cobro)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            cobro.CobroId = 0;
+            try
+            {
+                contexto.Cobros.Add(cobro);
+                paso = await contexto.SaveChangesAsync() > 0;
+
+                if (paso)
+                {
+                    foreach (var cobroDetalle in cobro.Detalles)
+                    {
+                        var venta = await VentasBLL.Buscar(cobroDetalle.VentaId);
+                        if (venta != null)
+                        {
+                            venta.Balance -= cobroDetalle.Monto;
+                            await VentasBLL.Modificar(venta);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                await contexto.DisposeAsync();
+            }
+
+            return paso;
+        }
+
+        public async static Task<bool> Modificar(Cobros cobro)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            await contexto.Database.ExecuteSqlRawAsync($"DELETE FROM CobroDetalle WHERE CobroId = {cobro.CobroId}");
+            foreach (var cobroDetalle in cobro.Detalles)
+            {
+                contexto.Entry(cobro).State = EntityState.Added;
+            }
+            try
+            {
+                contexto.Entry(cobro).State = EntityState.Modified;
+
+                paso = await contexto.SaveChangesAsync() > 0;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                await contexto.DisposeAsync();
+            }
+            return paso;
+        }
+
+        public async static Task<bool> Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var cobro = await Buscar(id);
+
+                if (cobro != null)
+                {
+                    contexto.Cobros.Remove(cobro);
+                    paso = await contexto.SaveChangesAsync() > 0;
+
+                    if (paso)
+                    {
+                        foreach (var cobroDetalle in cobro.Detalles)
+                        {
+                            var venta = await VentasBLL.Buscar(cobroDetalle.VentaId);
+                            if (venta != null)
+                            {
+                                venta.Balance += cobroDetalle.Monto;
+                                await VentasBLL.Modificar(venta);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                await contexto.DisposeAsync();
+            }
+
+            return paso;
+        }
+
+        public async static Task<Cobros> Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Cobros cobro;
+
+            try
+            {
+                cobro = await contexto.Cobros
+                    .Include(c => c.Detalles)
+                    .Where(v => v.CobroId == id)
+                    .FirstOrDefaultAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                await contexto.DisposeAsync();
+            }
+
+            return cobro;
+        }
+
+
+        public async static Task<bool> Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = await contexto.Cobros.AnyAsync(v => v.CobroId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                await contexto.DisposeAsync();
+            }
+
+            return encontrado;
+        }
+
+        public async static Task<List<Cobros>> GetCobros()
+        {
+            Contexto contexto = new Contexto();
+
+            List<Cobros> cobros = new List<Cobros>();
+            await Task.Delay(01); 
+
+            try
+            {
+                cobros = await contexto.Cobros.Include(c => c.Detalles).ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                await contexto.DisposeAsync();
+            }
+
+            return cobros;
+
+        }
     }
 }
